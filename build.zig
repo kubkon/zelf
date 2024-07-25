f97e13a11c6e5b49c1a8dc55f88d3c25f5373990
Jakub Konka <kubkon@jakubkonka.com>
2024-07-25 10:38:58 +0200

    update to latest zig master

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "zelf",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = mode,
         .use_llvm = use_llvm,
